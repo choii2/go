0c54f85d95952ac00fe48728ee7cca4f865fc77c
arrain2 <snowindsharu@yahoo.co.jp>
2018-05-02 00:04:37 +0900

    photon controller追加で動作チェック

---
+++ client/Assets/Scripts/PhotonController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Photonに接続する
+		PhotonNetwork.ConnectUsingSettings(null);
+	}
+
+	// ロビーに入ると呼ばれる
+	void OnJoinedLobby() {
+		Debug.Log("ロビーに入りました");
+	}
+
+}
