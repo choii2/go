dc2a9876b42f63b25674061885d622b66bec2120
arrain2 <snowindsharu@yahoo.co.jp>
2018-05-03 00:29:51 +0900

    プレイヤーが同じルームに入るように設定

--- client/Assets/Scripts/PhotonController.cs
+++ client/Assets/Scripts/PhotonController.cs
@@ -10,9 +10,27 @@ public class PhotonController : MonoBehaviour {
 		PhotonNetwork.ConnectUsingSettings(null);
 	}
 
-	// ロビーに入ると呼ばれる
+	// ロビー入室時イベント
 	void OnJoinedLobby() {
 		Debug.Log("ロビーに入りました");
+
+		PhotonRoomModel roomModel = new PhotonRoomModel();
+		// ルームの作成もしくはジョイン
+		PhotonNetwork.JoinOrCreateRoom(roomModel.roomName, roomModel.roomOptions, roomModel.typedLobby);
 	}
 
+	// ルーム作成時イベント
+	void OnCreatedRoom() {
+		Debug.Log("ルームを作成しました");
+	}
+
+	// ルームジョイン時イベント
+	void OnJoinedRoom() {
+		Debug.Log("ルームにジョインしました");
+	}
+
+	// 誰かがルームにジョイン時イベント
+	void OnPhotonPlayerConnected(PhotonPlayer newPlayer) {
+		Debug.Log("プレイヤー:"+ newPlayer.ID +"ルームに入室しました");
+	}
 }
---
+++ client/Assets/Scripts/PhotonRoomModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// ルームに関する情報を保持するモデルクラス
+public class PhotonRoomModel : MonoBehaviour {
+
+	private const string defaultRoomName = "test";
+
+	// ルーム名
+	public string roomName { get; private set; }
+	// ルームオプション
+	public RoomOptions roomOptions { get; private set; }
+	// どのロビーに作るか
+	public TypedLobby typedLobby { get; private set; }
+
+	public PhotonRoomModel() {
+		this.roomName = defaultRoomName;
+		this.roomOptions = new RoomOptions();
+		this.typedLobby = new TypedLobby();
+	}
+
+}
