a0fbc152b6a426cfe428f7740da4951604f80125
arrain2 <snowindsharu@yahoo.co.jp>
2018-05-03 15:33:17 +0900

    自分のオブジェクトのみ移動の制御を行う

--- client/Assets/Scripts/UnityChanController.cs
+++ client/Assets/Scripts/UnityChanController.cs
@@ -6,13 +6,27 @@ public class UnityChanController : MonoBehaviour {
     public Transform target;
     [SerializeField, HideInInspector] NavMeshAgent agent;
     [SerializeField, HideInInspector] Animator animator;
+    [SerializeField, HideInInspector] PhotonView photonView;
 
     void Start () {
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
+        photonView = GetComponent<PhotonView>();
     }
 
     void Update () {
+        // 自身の移動
+        Move();
+    }
+		
+	// 自身の移動処理
+    void Move() {
+        // 自分のオブジェクトではない場合には移動制御をしない
+        if (!photonView.isMine) {
+            return;
+        }
+
+        // 目的地に移動する
         agent.SetDestination(target.position);
         animator.SetFloat("Speed", agent.velocity.sqrMagnitude);
     }
