e4e1d38211798d2c8d31241f3b1968ea613677e1
arrain2 <snowindsharu@yahoo.co.jp>
2018-05-03 17:40:16 +0900

    コードフォーマッタ対応

--- client/Assets/Scripts/PhotonController.cs
+++ client/Assets/Scripts/PhotonController.cs
@@ -2,39 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PhotonController : MonoBehaviour {
+public class PhotonController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		// Photonに接続する
-		PhotonNetwork.ConnectUsingSettings(null);
-	}
+    // Use this for initialization
+    void Start ()
+    {
+        // Photonに接続する
+        PhotonNetwork.ConnectUsingSettings (null);
+    }
 
-	// ロビー入室時イベント
-	void OnJoinedLobby() {
-		Debug.Log("ロビーに入りました");
+    // ロビー入室時イベント
+    void OnJoinedLobby ()
+    {
+        Debug.Log ("ロビーに入りました");
 
-		PhotonRoomModel roomModel = new PhotonRoomModel();
-		// ルームの作成もしくはジョイン
-		PhotonNetwork.JoinOrCreateRoom(roomModel.roomName, roomModel.roomOptions, roomModel.typedLobby);
-	}
+        PhotonRoomModel roomModel = new PhotonRoomModel ();
+        // ルームの作成もしくはジョイン
+        PhotonNetwork.JoinOrCreateRoom (roomModel.roomName, roomModel.roomOptions, roomModel.typedLobby);
+    }
 
-	// ルーム作成時イベント
-	void OnCreatedRoom() {
-		Debug.Log("ルームを作成しました");
-	}
+    // ルーム作成時イベント
+    void OnCreatedRoom ()
+    {
+        Debug.Log ("ルームを作成しました");
+    }
 
-	// ルームジョイン時イベント
-	void OnJoinedRoom() {
-		Debug.Log("ルームにジョインしました");
+    // ルームジョイン時イベント
+    void OnJoinedRoom ()
+    {
+        Debug.Log ("ルームにジョインしました");
 
-		// TODO 初期値マジックナンバー対応
-		PhotonNetwork.Instantiate("SD_unitychan_humanoid", new Vector3(0, 0, 0), Quaternion.identity, 0);
-		PhotonNetwork.Instantiate("target", new Vector3(20, 5, -2), Quaternion.identity, 0);
-	}
+        // TODO 初期値マジックナンバー対応
+        PhotonNetwork.Instantiate ("SD_unitychan_humanoid", new Vector3 (0, 0, 0), Quaternion.identity, 0);
+        PhotonNetwork.Instantiate ("target", new Vector3 (20, 5, -2), Quaternion.identity, 0);
+    }
 
-	// 誰かがルームにジョイン時イベント
-	void OnPhotonPlayerConnected(PhotonPlayer newPlayer) {
-		Debug.Log("プレイヤー:"+ newPlayer.ID +"ルームに入室しました");
-	}
+    // 誰かがルームにジョイン時イベント
+    void OnPhotonPlayerConnected (PhotonPlayer newPlayer)
+    {
+        Debug.Log ("プレイヤー:" + newPlayer.ID + "ルームに入室しました");
+    }
 }
--- client/Assets/Scripts/UnityChanController.cs
+++ client/Assets/Scripts/UnityChanController.cs
@@ -2,32 +2,36 @@
 using UnityEngine.AI;
 using System.Collections;
 
-public class UnityChanController : MonoBehaviour {
+public class UnityChanController : MonoBehaviour
+{
     public Transform target;
     [SerializeField, HideInInspector] NavMeshAgent agent;
     [SerializeField, HideInInspector] Animator animator;
     [SerializeField, HideInInspector] PhotonView photonView;
 
-    void Start () {
-        agent = GetComponent<NavMeshAgent>();
-        animator = GetComponent<Animator>();
-        photonView = GetComponent<PhotonView>();
+    void Start ()
+    {
+        agent = GetComponent<NavMeshAgent> ();
+        animator = GetComponent<Animator> ();
+        photonView = GetComponent<PhotonView> ();
     }
 
-    void Update () {
+    void Update ()
+    {
         // 自身の移動
-        Move();
+        Move ();
     }
 		
-	// 自身の移動処理
-    void Move() {
+    // 自身の移動処理
+    void Move ()
+    {
         // 自分のオブジェクトではない場合には移動制御をしない
         if (!photonView.isMine) {
             return;
         }
 
         // 目的地に移動する
-        agent.SetDestination(target.position);
-        animator.SetFloat("Speed", agent.velocity.sqrMagnitude);
+        agent.SetDestination (target.position);
+        animator.SetFloat ("Speed", agent.velocity.sqrMagnitude);
     }
 }
