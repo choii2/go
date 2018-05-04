b2dded4f19dde432787b747a647087d334be6888
taketani-ken <takekanik2@gmail.com>
2018-05-04 19:58:01 +0900

    マスターキャラの追加

---
+++ client/Assets/Scripts/MasterCharaController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MasterCharaController : MonoBehaviour {
+
+	private Animator animator;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("up")) {
+			transform.position += transform.forward;
+			animator.SetBool("is_running", true);
+		} else {
+			animator.SetBool("is_running", false);
+		}
+		if (Input.GetKey("right")) {
+			transform.Rotate(0, 10, 0);
+		}
+		if (Input.GetKey ("left")) {
+			transform.Rotate(0, -10, 0);
+		}
+	}
+}
---
+++ client/Assets/Scripts/ServantCharaController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class ServantCharaController : MonoBehaviour
+{
+    public Transform Target;
+    private  NavMeshAgent _agent;
+    private Animator _animator;
+    private PhotonView _photonView;
+
+    private void Start ()
+    {
+        _agent = GetComponent<NavMeshAgent> ();
+        _animator = GetComponent<Animator> ();
+        _photonView = GetComponent<PhotonView> ();
+    }
+
+    private void Update ()
+    {
+        // 自身の移動
+        Move ();
+    }
+		
+    // 自身の移動処理
+    private void Move ()
+    {
+        // 自分のオブジェクトではない場合には移動制御をしない
+        if (_photonView && !_photonView.isMine) {
+            return;
+        }
+
+        // 目的地に移動する
+        _agent.SetDestination (Target.position);
+        _animator.SetFloat ("Speed", _agent.velocity.sqrMagnitude);
+    }
+}
